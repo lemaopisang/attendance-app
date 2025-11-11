69df895f82f082dae64b73b505b1259715457f5f
M. Arkandi Syahputra Santosa <138979992+lemaopisang@users.noreply.github.com>
2025-11-11 09:40:52 +0700

    Day 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:attendance_app/ui/home_screen.dart';
+import 'package:attendance_app/ui/absent/absent_screen.dart';
+import 'package:attendance_app/ui/attend/attend_screen.dart';
+import 'package:attendance_app/ui/attendance/attendance_history_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,38 +31,15 @@ Future<void> main() async {
 }
 
 class TestApp extends StatelessWidget {
-  // Main App
-  const TestApp({super.key}); // Constructor of TestApp clas
-
-  @override // can give information about about your missing override code
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // remove debug banner
-      home: const HomeScreen(), // HomeScreen class
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const TestApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50, // Header
-            child: Placeholder(),
-          ),
-          Expanded(
-            child: Placeholder(), // Content
-          ),
-          SizedBox(
-            height: 50, // Footer
-            child: Placeholder(),
-          ),
-        ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Attendance App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
     );
   }
